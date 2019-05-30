9a95def339ddd52080d593370acc3775a9c777b5
chenxianfeng <gaoyan20chen@foxmail.com>
2019-05-30 16:11:08 +0800

    530 from dahan 解决post跨域的问题

--- localhost.js
+++ localhost.js
@@ -2,3 +2,11 @@ var localhost= {
 	localhost:"http://localhost:3000",
 	ngrok:"https://chan.ngrok.xiaomiqiu.cn"
 }
+function getlocalhost(){
+	if (document.location.protocol == "file:"){
+		var url = localhost.localhost;
+	}else{
+		var url = localhost.ngrok;
+	}
+    return url;
+}
