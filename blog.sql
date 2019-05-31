05af96f811b443b4e1d2ea038041ff3b1a89c25f
chenxianfeng <gaoyan20chen@foxmail.com>
2019-05-31 17:48:32 +0800

    2019/05/31/17:48:32.75  from dahan 快捷更新

--- blog.sql
+++ blog.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50134
 File Encoding         : 65001
 
-Date: 2019-05-30 17:22:38
+Date: 2019-05-31 17:47:55
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -28,28 +28,16 @@ CREATE TABLE `blog` (
   `change_date` datetime DEFAULT NULL,
   `type` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`iid`)
-) ENGINE=InnoDB AUTO_INCREMENT=99 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=207 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of blog
 -- ----------------------------
 INSERT INTO `blog` VALUES ('1', 'Git使用教程', '', '<h3>1,git的安装</h3>2，git的使用', '2019-05-24 11:43:09', null, 'git');
 INSERT INTO `blog` VALUES ('67', 'java学习第一天', '123', '<h3 id=\"\">标题三</h3>\n<p>1233455</p>\n<h5 id=\"111111\">111111</h5>\n<p>1111</p>', '2019-05-29 14:09:10', '2019-05-29 14:09:10', 'test');
-INSERT INTO `blog` VALUES ('84', '13', '13', '<p>123</p>', '2019-05-30 16:15:07', '2019-05-30 16:15:07', null);
-INSERT INTO `blog` VALUES ('85', '1', '', '', '2019-05-30 16:18:56', '2019-05-30 16:18:56', null);
-INSERT INTO `blog` VALUES ('86', '1', '', '', '2019-05-30 16:21:49', '2019-05-30 16:21:49', null);
-INSERT INTO `blog` VALUES ('87', '123', '', '', '2019-05-30 17:04:58', '2019-05-30 17:04:58', null);
-INSERT INTO `blog` VALUES ('88', '1312', '', '', '2019-05-30 17:06:11', '2019-05-30 17:06:11', null);
-INSERT INTO `blog` VALUES ('89', '123', '', '', '2019-05-30 17:08:13', '2019-05-30 17:08:13', null);
-INSERT INTO `blog` VALUES ('90', '123', '', '', '2019-05-30 17:13:52', '2019-05-30 17:13:52', null);
-INSERT INTO `blog` VALUES ('91', '132', '', '', '2019-05-30 17:14:33', '2019-05-30 17:14:33', null);
-INSERT INTO `blog` VALUES ('92', '123131', '', '<p>13</p>', '2019-05-30 17:15:08', '2019-05-30 17:15:08', null);
-INSERT INTO `blog` VALUES ('93', '123', '', '', '2019-05-30 17:15:19', '2019-05-30 17:15:19', null);
-INSERT INTO `blog` VALUES ('94', '12313', '', '', '2019-05-30 17:15:59', '2019-05-30 17:15:59', null);
-INSERT INTO `blog` VALUES ('95', '123', '', '', '2019-05-30 17:16:56', '2019-05-30 17:16:56', null);
-INSERT INTO `blog` VALUES ('96', '12313', '', '<p>1321313</p>', '2019-05-30 17:17:10', '2019-05-30 17:17:10', null);
-INSERT INTO `blog` VALUES ('97', '111', '', '', '2019-05-30 17:18:15', '2019-05-30 17:18:15', null);
-INSERT INTO `blog` VALUES ('98', '11', '', '', '2019-05-30 17:19:29', '2019-05-30 17:19:29', null);
+INSERT INTO `blog` VALUES ('204', '这个是一个', '这个是一个', '<p>这个是一个</p>', '2019-05-31 11:59:46', '2019-05-31 11:59:46', null);
+INSERT INTO `blog` VALUES ('205', '手把手教你用git', '', '<h4 id=\"git\">一、如何安装git</h4>\n<p>下载地址：\nhttps://git-scm.com/download/win\n根据自己的电脑选择是32位的还是64位的。下载完后直接运行，之后一直next就好了。安装成功后，会有这样一个标志。</p>\n<h4 id=\"git-1\">二、如何使用git</h4>\n<p>（1）首先你要点进去https://github.com/这个网站，注册一个github账号。注册好之后，要记住邮箱和密码。</p>\n<p>（2） 打开之前的那个图标就是这样一个窗口，首先Git是分布式版本控制系统，所以需要填写用户名和邮箱作为一个标识，分别输入这两个命令，用户名和邮箱需要换成自己的。</p>\n<p>（3）  打开之前的那个图标就是这样一个窗口。现在比如我在我的C:/wamp/www/aaa有一个项目，我要对它进行管理怎么办呢。直接在控制台里面输入  cd   c:/wamp/www/aaa  之后你就会在aaa这个目录里面了。那如何对它进行管理呢。直接在控制台输入git  init</p>\n<p>之后你去查看aaa目录里面发现里面多了这样一个东西。</p>\n<p>这是一个隐藏文件夹，有点电脑看不见，是因为文件夹选项设置的隐藏文件不可见，这个去控制面板里的文件夹选择设置一下就好了。</p>\n<p>下面我们接着来操作，试着开始写我们的项目。</p>\n<p>首先我写一个首页叫index.html</p>', '2019-05-31 15:48:05', '2019-05-31 15:48:05', 'git');
+INSERT INTO `blog` VALUES ('206', '这和和', '这和和', '<p>这和和</p>', '2019-05-31 17:28:27', '2019-05-31 17:28:27', null);
 
 -- ----------------------------
 -- Table structure for picture
@@ -64,3 +52,28 @@ CREATE TABLE `picture` (
 -- ----------------------------
 -- Records of picture
 -- ----------------------------
+
+-- ----------------------------
+-- Table structure for type
+-- ----------------------------
+DROP TABLE IF EXISTS `type`;
+CREATE TABLE `type` (
+  `iid` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `blog` int(11) DEFAULT NULL,
+  PRIMARY KEY (`iid`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of type
+-- ----------------------------
+INSERT INTO `type` VALUES ('1', 'Java web', '1111');
+INSERT INTO `type` VALUES ('2', 'eclipse', '11');
+INSERT INTO `type` VALUES ('3', '数据库', '11111');
+INSERT INTO `type` VALUES ('4', 'Git', '1');
+INSERT INTO `type` VALUES ('5', 'Python', '1111');
+INSERT INTO `type` VALUES ('6', 'Node.js', null);
+INSERT INTO `type` VALUES ('7', 'Vue.js', null);
+INSERT INTO `type` VALUES ('8', '小程序', null);
+INSERT INTO `type` VALUES ('9', '其他', null);
+INSERT INTO `type` VALUES ('10', '收藏夹', null);
